f92835cfe95fa8e0039384df91801bb022221d75
taumol <46528949+taumol@users.noreply.github.com>
2023-04-25 17:55:36 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -130,7 +130,8 @@ public class ApiConfig {
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
         // Embedded Source : Update in Strings.xml if required
-        String apiUrl = Hawk.get(HawkConfig.API_URL, HomeActivity.getRes().getString(R.string.app_source));
+        //         String apiUrl = Hawk.get(HawkConfig.API_URL, HomeActivity.getRes().getString(R.string.app_source));
+        String apiUrl = Hawk.get(HawkConfig.API_URL,"https://raw.iqiq.io/taumol/HopeTV/main/HopeTV.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
